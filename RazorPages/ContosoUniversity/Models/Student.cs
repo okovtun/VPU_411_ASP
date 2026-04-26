4d2b31e6c570127126a46901fe15491f4dc4964e
User <User@DESKTOP-QHG18FL>
2026-04-26 14:23:33 +0300

    RazorPages start

---
+++ RazorPages/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		//Navigation properties:
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
