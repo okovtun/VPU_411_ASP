20a2d8cebaf50c6109919a4519d03ef90b053387
User <User@DESKTOP-QHG18FL>
2026-05-24 12:34:00 +0300

    All models DONE

--- RazorPages/ContosoUniversity/Models/Course.cs
+++ RazorPages/ContosoUniversity/Models/Course.cs
@@ -7,10 +7,18 @@ namespace ContosoUniversity.Models
 	{
 		[DatabaseGenerated(DatabaseGeneratedOption.None)] //Выключаем автоикремент
 		public int CourseID { get; set; }
+
+		[StringLength(50, MinimumLength = 3)]
 		public string Title { get; set; }
+
+		[Range(0, 5)]
 		public int Credits { get; set; }
 
+		public int DepartmentID { get; set; }	//Внешний ключ
+
 		//Navigation properties:
+		public Department Department { get; set; }
+		public ICollection<Instructor> Instructors { get; set; }
 		public ICollection<Enrollment> Enrollments { get; set; }
 	}
 }
---
+++ RazorPages/ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+	public class Instructor
+	{
+		public int ID { get; set; }
+
+		[Required(ErrorMessage = "Без фамилии никак...")]
+		[StringLength(50)]
+		[RegularExpression(@"^[A-Z А-Я]+[a-z а-я]*$", ErrorMessage = "Введены недопустимые символы")]
+		[Display(Name = "Фамилия")]
+		public string LastName { get; set; }
+		
+		[Required(ErrorMessage = "Без имени никак...")]
+		[StringLength(50)]
+		[RegularExpression(@"^[A-Z А-Я]+[a-z а-я]*$", ErrorMessage = "Введены недопустимые символы")]
+		[Display(Name = "Имя")]
+		public string FirstName { get; set; }
+
+		[DataType(DataType.Date)]
+		[Display(Name = "Дата трудоустройства")]
+		public DateTime HireDate { get; set; }
+
+		[Display(Name = "Преподаватель")]
+		public string FullName { get => $"{LastName} {FirstName}"; }
+
+		//Navigation proprties:
+		public OfficeAssignment OfficeAssignment { get; set; }
+		public ICollection<Course> Courses { get; set; }
+	}
+}
